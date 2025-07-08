d75522a524e29de7d1c99f9273403339ea155cef
Agure-la <lameckagure@gmail.com>
2025-07-08 15:20:55 +0300

    added page to show create new resource

--- lib/blog_web/controllers/article_controller.ex
+++ lib/blog_web/controllers/article_controller.ex
@@ -10,4 +10,21 @@ defmodule BlogWeb.ArticleController do
         article = Blog.MyBlog.get_article!(id)
         render(conn, :show, article: article)
     end
+
+    def new(conn, _params) do
+        changeset = Blog.MyBlog.change_article(%Article{})
+        render(conn, :new, changeset: changeset)
+    end
+
+    def create(conn, _params) do
+        case Blog.MyBlog.create_article(%{title: "...", body: "..."}) do
+            {:ok, article} ->
+            conn
+            |> put_flash(:info, "Article created successfully.")
+            |> redirect(to: ~p"/articles/#{article})
+
+            {:error, %Ecto.changeset{} = changeset} ->
+            render(conn, :new, changeset: changeset)
+        end
+    end
 end
